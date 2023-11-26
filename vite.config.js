d5b762ecfb295895b2400c3f552a3317569df9f8
JeffErasLindao <jeras@espol.edu.ec>
2023-11-26 11:37:47 -0500

    se anadieron elementos al portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portafolio-videojuegos/',
+  base: 'https://jefferaslindao.github.io/portafolio-videojuegos/',
   plugins: [vue()],
 })
